cb2ec6779306400447ca6c6cf193bf0153b8546c
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-03-18 00:39:59 +0000

    Prints numbers from one to nine

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - This is the entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int r;
+
+	for (r = 0; r <= 9; r++)
+		printf("%d", r);
+	printf("\n");
+	return (0);
+}
