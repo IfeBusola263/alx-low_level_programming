366c7a92d7b70ac5391b4c6300ebf55e1a6a3431
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-03-21 23:35:54 +0000

    Addition of two ints

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - This is the entry point
+ * @k: Formal Parameter
+ * @l: Formal parameter
+ *
+ * Return: returns an int
+ */
+
+int add(int k, int l)
+{
+	int m;
+
+	m = k + l;
+
+	return (m);
+}
