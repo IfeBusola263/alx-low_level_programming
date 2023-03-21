77e7b7d72617c0d6a2c4ee7e87a3e94a97d3a10c
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-03-21 18:17:46 +0000

    Prototype for alphabet program

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - This is the entry point
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+}
