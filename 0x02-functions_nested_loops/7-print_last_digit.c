2bd84f8622516d5a1a5749492e0c3e67b87f4cfd
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-03-21 21:57:25 +0000

    Prints the last number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - This is the entry point
+ *
+ * @l: This is the formal parameter
+ *
+ * Return: returns an int
+ */
+
+int print_last_digit(int l)
+{
+	int last;
+
+	last = l % 10;
+
+	return (last);
+}
