287d3f6ecc3c22e12eba84235fb3151d6c1f2625
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-03-21 20:51:06 +0000

    Checks if c is an alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - this is the entry point
+ *
+ * @c: This is the formal argument
+ *
+ * Return: returns 1 if true, zero there wise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 41 && c <= 90) || ((c >= a) && (c <= 122))
+
+			return (1);
+
+	else
+
+			return (0)
+
+}
