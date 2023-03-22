6c37241eb632f0fa85c2ed4c763b8f88a4cc9293
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-03-22 00:03:50 +0000

    Prints natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - This is the entry point
+ * @n: The formal parameter
+ *
+ * Return: returns void
+ */
+
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		printf("%d, ", n);
+	}
+}
