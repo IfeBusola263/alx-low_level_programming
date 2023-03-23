db61f0697ea654c1b847e6a950e6b0d1499564af
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-03-23 19:51:46 +0000

    Multipy two integars

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - This is the entry pont
+ *
+ * @a: the formal argument
+ * @b: the formal argument
+ *
+ * Return: returns an int
+ */
+int mul(int a, int b)
+{
+	long int m;
+
+	m = a * b;
+
+	return (m);
+}
