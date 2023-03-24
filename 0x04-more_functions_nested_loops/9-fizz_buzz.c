5a9af693e628d412f0eca8be860c40251130acad
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-03-24 01:57:05 +0000

    Fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,23 +9,23 @@
 int main(void)
 {
 	int i;
-	char j[] = "Fizz";
-	char k[] = "Buzz";
-	char l[] = "FizzBuzz";
+	char j[] = "Fizz ";
+	char k[] = "Buzz ";
+	char l[] = "FizzBuzz ";
 
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
 		{
-			printf("%s ", l);
+			printf("%s", l);
 		}
 		else if (i % 5 == 0)
 		{
-			printf("%s ", k);
+			printf("%s", k);
 		}
 		else if (i % 3 == 0)
 		{
-			printf("%s ", j);
+			printf("%s", j);
 		}
 		else
 		{
