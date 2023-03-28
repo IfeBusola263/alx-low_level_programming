0d01519ec9152ab93eb291d5827334fa00ddacfe
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-03-28 04:21:14 +0000

    Prints reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev- prints to standard output with newline
+ * @s: point to the string
+ *
+ * Return: returns nothing
+ */
+
+void print_rev(char *s)
+{
+	int c;
+	int check = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		check++;
+	}
+	for (c = check; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+	_putchar('\n');
+}
