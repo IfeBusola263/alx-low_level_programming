3e40a06fa5770454ad8a9dcf64367c3e11253f8a
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-03-28 19:22:32 +0000

    copy strings from source to destination

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,11 +12,19 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
+	int j;
 
+	j = 0;
+	while (src[j] != '\0')
+	{
+		j++;
+	}
+	
 	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
+	dest[j] = '\0';
 	return (dest);
 }
 
