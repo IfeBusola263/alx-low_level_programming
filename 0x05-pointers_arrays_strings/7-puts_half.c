b3803c335a0c75a50624f21caea77ef181247abc
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-03-28 04:58:31 +0000

    Update repo

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+ /**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: pointer to str
+ *
+ * Return: returns nothing
+ */
+ 
+void puts_half(char *str)
+{
+
+}
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+ /**
+ * print_array - prints half of a string, followed by a new line
+ * @a: pointer to str
+ * @n: pointer
+ *
+ * Return: returns nothing
+ */
+ 
+void print_array(int *a, int n)
+{
+
+}
