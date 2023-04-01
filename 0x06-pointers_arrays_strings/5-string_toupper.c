4297da695dbaff3ac9a747d5ba9b1b1fcecf9c9d
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-04-01 01:50:58 +0000

    Switches cases

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - Changes lowercase str to uppercase str
+ * @a: parameter type declaration
+ *
+ * Return: returns the mem address of the first achr of string
+ */
+
+char *string_toupper(char *a)
+{
+	int itr;
+	char a;
+
+	itr = 0;
+	while (a[itr] != '\0')
+	{
+		if (a[itr] >= 'a' && a[itr] <= 'z')
+			a[itr] = a[itr] - 32;
+		itr++;
+	}
+	return (a);
+}
