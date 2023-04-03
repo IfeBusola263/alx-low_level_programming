09b3ed1945fb310cf07e0eadb399aec555d19224
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-04-03 22:08:58 +0000

    copies strings of specific byte

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: memory address to be copied to
+ * @src: source array being copied from
+ * @n: number of bytes to be copied
+ *
+ * Return: returns a pointer to destination array
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int itr;
+
+	itr = 0;
+	while (src[itr] != '\0' && src[itr] <= n)
+	{
+		dest[itr] = src[itr];
+		itr++;
+	}
+
+	return (0);
+}
