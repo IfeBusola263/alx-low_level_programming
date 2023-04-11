5569588ce9c98d31a6b8711b2dcace46a4f01426
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-04-11 16:42:36 +0000

    get concatenated string

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - returns a pointer to a concatenated string
+ * @s1: destination string
+ * @s2: source string
+ *
+ * Return: pointer to concatenated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, k, l;
+	char *p; /* pointer to return concatenated string */
+
+	if (s1 == NULL) /* if args are NULL treat as empty string */
+		*s1 = ' ';
+	if (s2 == NULL)
+		*s2 = ' ';
+
+	i = 0;
+	while (s1[i] != '\0')
+		i++;
+
+	j = 0;
+	while (s2[j] != '\0')
+		j++;
+
+	p = malloc(i + j + 1);
+
+	k = 0;
+	while (k < i)
+	{
+		if (p == NULL)
+			return (NULL);
+
+		p[k] = s1[k];
+		k++;
+	}
+
+	l = 0;
+	while (k < (i + j))
+	{
+		p[k] = s2[l];
+		k++;
+		l++;
+	}
+	p[i + j] = '\0';
+	return (p);
+}
