0c86fc0c50975f628b4b2a92304f7eafffb55b1c
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-04-11 16:44:48 +0000

    get concatenated string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,9 @@ char *str_concat(char *s1, char *s2)
 	char *p; /* pointer to return concatenated string */
 
 	if (s1 == NULL) /* if args are NULL treat as empty string */
-		*s1 = ' ';
+		*s1 = " ";
 	if (s2 == NULL)
-		*s2 = ' ';
+		*s2 = " ";
 
 	i = 0;
 	while (s1[i] != '\0')
