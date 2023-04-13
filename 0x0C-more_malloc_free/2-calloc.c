fc6ec3355cc96d6c4b3178e28057c7875a04d912
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-04-13 16:37:39 +0000

    Calloc template

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: number of mumbers to the array
+ * @size: size of type memory is bring allocated for
+ *
+ * Return: returns a pointer to the new array in memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p; /* pointer to return */
+	unsigned int i; /* iterator */
+
+	/* Error handling for input for nmemb */
+	if (nmemb <= 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		p[i] = 0;
+
+	return (p);
+}
