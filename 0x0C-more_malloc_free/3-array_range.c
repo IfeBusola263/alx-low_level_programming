1b0de2d4ce7128499b9010e9ec927807d2a27cdb
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-04-13 18:40:49 +0000

    Get range of values from min to max

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * array_range - creates an array of integers
+ * @min: mininum value
+ * @max: maximum value
+ *
+ * Return: returns a pointer to an array
+ */
+
+int *array_range(int min, int max)
+{
+	int i;
+	int *p;
+	int total_bytes;
+
+	total_bytes = max - min + 1;
+	
+	if (min > max || total_bytes <= 0)
+		return (NULL);
+
+	p = malloc(sizeof(int) * total_bytes);
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < total_bytes; i++)
+	{
+		p[i] = min  + i;
+	}
+
+	return (p);
+}
+
