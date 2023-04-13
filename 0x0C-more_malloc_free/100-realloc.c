abed8aa8c2ef75889ee6bcbfdc05d9769b5c236c
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-04-13 20:48:37 +0000

    Get reallocated memory block

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -35,7 +35,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	for (i = 0; i < old_size; i++)
 		*((char *)ptr + i) = *((char *)p + i);
-	
+
 	free(ptr);
 	return (p);
 }
