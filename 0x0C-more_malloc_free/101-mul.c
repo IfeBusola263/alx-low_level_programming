958936d1b36923cd25fa8d16370e717018f8b054
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-04-14 13:51:01 +0000

    Correct mul function

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ * print_error - prints error to stdot
+ *
+ * Return: returns nothing
+ */
+void print_error(void)
+{
+	char error[] = "Error";
+	int i;
+
+	for (i = 0; error[i] != '\0'; i++)
+		putchar(error[i]);
+}
 
 /**
  * main - get mutiplication of two numbers
@@ -14,35 +27,28 @@ int main(int argc, char *argv[])
 	int i;
 	long mul;
 	long result;
+	char *stop;
 
 	mul = 1;
 
 	if (argc < 3)
 	{
-		putchar('E');
-		putchar('r');
-		putchar('r');
-		putchar('o');
-		putchar('r');
+		print_error();
 		putchar('\n');
 		exit(98);
 	}
 
 	for (i = 1; i < argc; i++)
 	{
-		result = strtol(argv[i], NULL, 10);
-		if (result == 0)
+		result = strtol(argv[i], &stop, 10);
+		if (*stop)
 		{
-			putchar('E');
-			putchar('r');
-			putchar('r');
-			putchar('o');
-			putchar('r');
+			print_error();
 			putchar('\n');
 			exit(98);
 		}
-		mul = mul * result;
 
+		mul = mul * result;
 	}
 
 	printf("%ld\n", mul);
