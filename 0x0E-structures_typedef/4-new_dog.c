91813bb0746eee00e1c74cb206468c708afc4bdb
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-04-18 10:13:41 +0000

    Correct 4-new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,7 +17,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
+	{
+		free(ptr);
 		return (NULL);
+	}
 
 	ptr->name = name;
 	ptr->age = age;
