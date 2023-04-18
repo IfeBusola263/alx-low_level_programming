40c601163f7f2c96532404245325c2a00cde9661
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-04-18 13:31:31 +0000

    Correct new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,42 @@
 #include <stdio.h>
 
 /**
+ * copy - Copies a string to another memory address
+ * @dest: destimation address
+ * @src: source of string(address)
+ *
+ * Return: returns a pointer to string
+ */
+
+char *copy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * len - Gives length of string
+ * @c: pointer to string
+ *
+ * Return: returns an integer
+ */
+
+int len(char *c)
+{
+	int i = 0;
+
+	for (i = 0; c[i] != '\0';)
+		i++;
+	return (i);
+}
+
+
+/**
  * new_dog - creates a new dog
  * @name: first member
  * @age: second member
@@ -13,20 +49,31 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *ptr;
-	size_t i;
+	dog_t *ptr;;
 
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
 	{
-		for (i = 0; i < sizeof(dog_t); i++)
-			free(((char *)ptr + i));
+		free(ptr);
 		return (NULL);
 	}
 
-	ptr->name = name;
+	ptr->name = malloc(len(name) + 1);
+	if (ptr->name == NULL)
+	{
+		free(ptr->name);
+		return (NULL);
+	}
+	ptr->owner = malloc(len(owner) + 1);
+	if (ptr->owner == NULL)
+	{
+		free(ptr->owner);
+		return (NULL);
+	}
+
+	copy(ptr->name, name);
+	copy(ptr->owner, owner);
 	ptr->age = age;
-	ptr->owner = owner;
 
 	return (ptr);
 }
