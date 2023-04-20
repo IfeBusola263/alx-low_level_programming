2b1b43c9f555b2db7e789b2f296af16ff17ced56
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-04-20 18:15:15 +0000

    Print strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_strings - Prints stringss
+ * @separator: delimiter
+ * @n: number of strings
+ *
+ * Return: returns nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *j;
+	va_list ops;
+
+	if (n <= 0)
+		return;
+
+	va_start(ops, n);
+
+	for (i = 0; i < n; i++)
+	{
+		j = va_arg(ops, char*);
+		printf("%s", j);
+		if (i != (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+		if (separator == NULL)
+			putchar(' ');
+	}
+	putchar('\n');
+
+	va_end(ops);
+}
