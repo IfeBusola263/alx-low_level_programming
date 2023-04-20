678b9475c605972fd17c3af850749b6b31dd14c6
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-04-20 16:54:23 +0000

    Print numbers with delimiter

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ops;
 
 	/* argument check */
-	if (n > 0 && separator != NULL)
+	if (n <= 0 && separator != NULL)
 		return;
 
 	va_start(ops, n);
