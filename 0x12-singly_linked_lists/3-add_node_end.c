a705bc638e3fd62881ef3d6072c7b2c7246abf52
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-04-27 21:29:47 +0000

    Print linked list

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * add_node_end - returns the address to the head
+ * @head: the pointer to the address coming into the func
+ * @str: List to be added to beginning of list
+ *
+ * Return: pointer to the head
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newAdd;
+	list_t *trav_ptr;
+
+	if (str == NULL || head == NULL)
+		return (NULL);
+
+	if (*head == NULL)
+	{
+		newAdd = malloc(sizeof(list_t));
+		if (newAdd == NULL)
+			return NULL;
+		newAdd->str = strdup(str);
+		newAdd->len = strlen(str);
+		newAdd->next = *head;
+		*head = newAdd;
+
+	} 
+	if (*head != NULL)
+	{
+		newAdd = malloc(sizeof(list_t));
+		if (newAdd == NULL)
+			return (NULL);
+		newAdd->str = strdup(str);
+		newAdd->len = strlen(str);
+		newAdd->next = NULL;
+
+		trav_ptr = *head;
+		while (trav_ptr->next)
+		{
+			trav_ptr = trav_ptr->next;
+		}
+		trav_ptr->next = newAdd;
+	}
+	return (newAdd);
+}
