c73fdc4b8e5a602f3a19d91432ad1c0bd0dade7a
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-04-28 19:15:36 +0000

    Print string in assembly language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,19 @@
 section .data
+	;The string i want to print is followed by a newline and the null byte
 	text db "Hello, Holberton", 10,0
 
 section .text
 	global _start
 
 _start:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, text
-	mov rdx, 14
+	; Use the syscall to write to stdot
+	mov rax, 1	;syscall number for write
+	mov rdi, 1	;file descriptor - stdot
+	mov rsi, text	;Pointer to the string to be printed
+	mov rdx, 16	;length of the string, newline  and null byte
 	syscall
 
-	mov rax, 60
-	xor rdi, rdi
+	;syscall to exit the program
+	mov rax, 60	; Exit number with syscall
+	xor rdi, rdi	; Exit point
 	syscall
