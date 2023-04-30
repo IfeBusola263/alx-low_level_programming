244386fb64ccc3b06cde0227f616ff4669db0fab
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-04-30 21:02:41 +0000

    Print content of node

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - prints the element of a list.
+ * @h: pointer to the head.
+ *
+ * Return: returns the number of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count;
+
+	count = 0;
+
+	if (h == NULL)
+		exit(98);
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
