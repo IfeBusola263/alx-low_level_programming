08cf2b02a09982f9373a13c11ad2b84caf090e01
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-05-01 15:41:28 +0000

    Insert node at nth position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,38 +9,44 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *checkPtr;
-	listint_t *newAdd;
+	listint_t *checkPtr, *newAdd;
 	unsigned int move;
 
 	if (head == NULL)
 		return (NULL);
 
-	/* check if index is available */
 	checkPtr = *head;
 	for (move = 0; checkPtr != NULL; move++)
 		checkPtr = checkPtr->next;
 
-	if (idx > move)
-		return (NULL);
-
-	/* move to index before the idx */
-	/* holding the address of node at idx */
-	checkPtr = *head;
-	move = 0;
-	while (move < idx - 1)
-	{
-		checkPtr = checkPtr->next;
-		move++;
-	}
-
 	newAdd = malloc(sizeof(listint_t));
 	if (newAdd == NULL)
 		return (NULL);
-
 	newAdd->n = n;
-	newAdd->next = checkPtr->next;
-	checkPtr->next = newAdd;
 
+	if (move < idx)
+	{
+		return (NULL);
+	}
+	else
+	{
+		if (idx == 0)
+		{
+			checkPtr = *head;
+			newAdd->next = checkPtr;
+			*head = newAdd;
+		} else
+		{
+			checkPtr = *head; /* move to index before the idx */
+			move = 0;	/* holding the address of node at idx */
+			while (move < idx - 1)
+			{
+				checkPtr = checkPtr->next;
+				move++;
+			}
+			newAdd->next = checkPtr->next;
+			checkPtr->next = newAdd;
+		}
+	}
 	return (newAdd);
 }
