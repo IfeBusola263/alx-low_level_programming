50ab9b753bf401b2cf7446b98d155f9a0edffde1
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-05-02 11:29:05 +0000

    reverse a linked list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * reverse_listint - reverse a linked list
+ * @head: pointer to a pointer pointing to the first node
+ *
+ * Return: pointer to the first node.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *ptrUHead;
+	listint_t *headUPtr;
+
+	if (*head == NULL)
+		return (NULL);
+
+	headUPtr = (*head)->next;
+	ptrUHead = headUPtr->next;
+	(*head)->next = NULL;
+
+	while (ptrUHead != NULL)
+	{
+		headUPtr->next = *head;
+		*head = ptrUHead->next;
+		ptrUHead->next = headUPtr;
+		headUPtr = (*head)->next;
+		(*head)->next = ptrUHead;
+		ptrUHead = headUPtr->next;
+	}
+	headUPtr->next = *head;
+	*head = headUPtr;
+
+	return (*head);
+}	
