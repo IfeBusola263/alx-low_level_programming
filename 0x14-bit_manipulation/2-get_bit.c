354938b217add77ee086c31d46867e3b16677160
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-05-04 18:36:19 +0000

    get bit at nth index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * get_bit - gets bit at a certain index
+ * @n: int to be converted to bit
+ * @index: index of bit to be gotten
+ *
+ * Return: returns an integer
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+
+	if (n == 0 || n == 1)
+		return (n);
+
+	n = n >> index;
+	i = n & 1;
+
+	return (i);
+}
