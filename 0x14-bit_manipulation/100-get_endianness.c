6570d77643a284043cfb79e551a1cc37451a4372
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-05-05 12:22:05 +0000

    Get endianness of arch

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * get_endianness - points the byte order of a computer
+ *
+ * Return: returns nothing
+ */
+int get_endianness(void)
+{
+	int unsigned *ptr;
+	unsigned int check = 1;
+
+	ptr = &check;
+
+	if (*ptr)
+		return (1);
+	else
+		return (0);
+	
+}
