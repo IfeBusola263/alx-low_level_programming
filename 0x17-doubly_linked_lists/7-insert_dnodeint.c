144e158aeccd4cb04c30c1e0bf2befe7ce790414
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-06-15 23:22:21 +0000

    correct Node addition

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -41,6 +41,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		newNode->prev = ptr->prev;
 		ptr->prev = newNode;
 		newNode->next = ptr;
+		ptr = NULL;
 		return (newNode);
 	}
 	return (NULL);
