e4fc4c0d0f0b1e3d7137b3a89059610114134bec
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-07-09 19:36:39 +0000

    Hash table for dictionary

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,12 +9,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
 
-	table = malloc(sizeof(hash_table_t));
+	table = malloc(sizeof(hash_table_t *) * size);
 
 	if (table == NULL)
 		return (NULL);
 
 	table->size = size;
+	table->array = NULL;
 
 	return (table);
 }
