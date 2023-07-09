133bbff1a74434dc12ac84e564f98cbd8f8b1ad2
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-07-09 21:17:09 +0000

    Index function for mapping items on hash table

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+/**
+ * int key_index - generates the index for an item on the table
+ * @key: main data which information would be mapped to
+ * @size: the size of the hash table
+ *
+ * Return: the index to put the data on the hash table
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int idx, hash;
+
+
+	hash = hash_djb2((unsigned char *) key);
+	idx = hash % size;
+
+	return (idx);
+}
