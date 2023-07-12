579198218468e8db14160a98a670f6710eb0217f
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-07-12 15:54:20 +0000

    Corrected the table creation task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,9 +16,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	table->array = malloc(sizeof(hash_node_t) * table->size);
+	table->array = malloc(sizeof(hash_node_t *) * table->size);
 	if (table->array == NULL)
+	{
+		free(table);
 		return (NULL);
+	}
 
 	for (i = 0; i < table->size; i++)
 	{
