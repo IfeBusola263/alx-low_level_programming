1a853b7e78a2a2b619ba0c256515cdc036179739
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-08-24 10:55:19 +0000

    Python script to calculate the perimeter of an island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+This module houses the function to calculate the perimeter of
+an Island
+'''
+
+def island_perimeter(grid):
+    '''
+    Returns the perimeter of an island in form of a
+    grid, which is a list of list of integers.
+    '''
+
+    if type(grid) is list:
+
+        height = 0
+        width = []
+        for unit in grid:
+
+            if type(unit) is list and 1 in unit:
+                height += 1
+                width.append(sum(unit))
+
+        return (height + max(width)) * 2
