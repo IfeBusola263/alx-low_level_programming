da1c420434e2ccf561f4d871a815e9154b135e02
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-10-12 15:57:09 +0000

    Advanced binary algorithm

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,61 @@
+#include "search_algos.h"
+/**
+ * recur_search - function recursively searches for the first
+ * @array: pointer to the array to be searched
+ * @top: lower index of the array
+ * @end: higher index
+ * @value: value to be searched
+ *
+ * Return: the index of value upon success, -1 otherwise
+ */
+int recur_search(int *array, size_t top, size_t end, int value)
+{
+	size_t i, mid;
+
+	if (top > end)
+		return (-1);
+
+	mid = (top + end) / 2;
+
+	printf("Searching in array: ");
+	for (i = top; i <= end; i++)
+	{
+		printf("%d", array[i]);
+		if (i < end)
+			printf(", ");
+	}
+	printf("\n");
+
+
+	if (array[mid] == value)
+	{
+		if (array[mid - 1] == value)
+			return (recur_search(array, top, mid, value));
+		return ((int)mid);
+	}
+	else if (array[mid] > value)
+		return (recur_search(array, top, mid - 1, value));
+	else
+		return (recur_search(array, mid + 1, end, value));
+}
+/**
+ * advanced_binary - The functions searches for a value with binary search
+ *
+ * @array: Pointer to the first element of an array
+ * @size: The number of elements in array
+ * @value: The value to be searched
+ *
+ * Return: the index of value upon success, -1 otherwise
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	size_t start, stop;
+
+	if (array == NULL) /* if the array is empty */
+		return (-1);
+
+	start = 0;
+	stop = size - 1;
+
+	return (recur_search(array, start, stop, value));
+}
